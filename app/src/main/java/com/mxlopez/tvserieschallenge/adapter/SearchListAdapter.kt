a19174763cccf44496473297c6f79df0ac21d5e8
Maximo Lopez <mlopez@oriontek.do>
2022-03-12 03:41:53 -0400

    SearchedShow model and SearchListAdapter added

---
+++ app/src/main/java/com/mxlopez/tvserieschallenge/adapter/SearchListAdapter.kt
@@ -0,0 +1,40 @@
+package com.mxlopez.tvserieschallenge.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.mxlopez.tvserieschallenge.R
+import com.mxlopez.tvserieschallenge.models.SearchedShow
+
+class SearchListAdapter(private val list: List<SearchedShow>?): RecyclerView.Adapter<SearchListAdapter.ViewHolder>() {
+    private lateinit var ctx: Context
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.main_list_cell, parent, false)
+        ctx = parent.context
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.seriesName.text = list?.get(position)!!.show?.name ?: "N/A"
+        holder.cellBody.setOnClickListener {
+            Toast.makeText(ctx, "${list[position].show?.name}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list?.size ?: 0
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var seriesImage: ImageView = itemView.findViewById(R.id.ivSeriesImage)
+        var seriesName: TextView = itemView.findViewById(R.id.tvSeriesName)
+        var cellBody: ConstraintLayout = itemView.findViewById(R.id.clCellBody)
+
+    }
+}
---
+++ app/src/main/java/com/mxlopez/tvserieschallenge/models/SearchedShow.kt
@@ -0,0 +1,6 @@
+package com.mxlopez.tvserieschallenge.models
+
+data class SearchedShow(
+    val score: Float = 0.0F,
+    val show: Show?
+)
