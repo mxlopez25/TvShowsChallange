405771176a4bb75eba5c1bdd00a576902cd1c78d
Maximo Lopez <mlopez@oriontek.do>
2022-03-13 22:38:35 -0400

    Favorites Fragment updated to handle data

--- app/src/main/java/com/mxlopez/tvserieschallenge/FavoritesFragment.kt
+++ app/src/main/java/com/mxlopez/tvserieschallenge/FavoritesFragment.kt
@@ -6,6 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mxlopez.tvserieschallenge.adapter.MainListAdapter
+import com.mxlopez.tvserieschallenge.databinding.FragmentFavoritesBinding
+import com.mxlopez.tvserieschallenge.databinding.FragmentSearchBinding
+import com.mxlopez.tvserieschallenge.models.Show
 import com.mxlopez.tvserieschallenge.repository.TvMazeApiRepository
 import com.mxlopez.tvserieschallenge.viewmodels.SharedViewModel
 import com.mxlopez.tvserieschallenge.viewmodels.SharedViewModelFactory
@@ -16,6 +21,9 @@ class FavoritesFragment : Fragment() {
     private lateinit var repository: TvMazeApiRepository
     private lateinit var sharedViewModelFactory: SharedViewModelFactory
 
+    private lateinit var binding: FragmentFavoritesBinding
+    private lateinit var adapter: MainListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,12 +33,30 @@ class FavoritesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val v = inflater.inflate(R.layout.fragment_favorites, container, false)
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         repository = TvMazeApiRepository()
         sharedViewModelFactory = SharedViewModelFactory(repository)
         sharedViewModel = ViewModelProvider(requireActivity(), sharedViewModelFactory).get(SharedViewModel::class.java)
+        sharedViewModel.fetchFavorites()
 
-        return v
+        val add: (Show) -> Unit = { s -> sharedViewModel.addToFavorites(s) }
+        val remove: (Show) -> Unit = { s -> sharedViewModel.removeFromFavorites(s) }
+
+//        adapter = MainListAdapter(sharedViewModel.favoriteShow.value, sharedViewModel.favoriteShow.value, add, remove)
+//        binding.rvFavsList.adapter = adapter
+
+        sharedViewModel.favoriteShow.observe(viewLifecycleOwner) {
+            adapter = MainListAdapter(it, sharedViewModel.favoriteShow.value, add, remove)
+            binding.rvFavsList.layoutManager = LinearLayoutManager(context)
+            binding.rvFavsList.adapter = adapter
+        }
+
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.notifyDataSetChanged()
     }
 
     companion object {
